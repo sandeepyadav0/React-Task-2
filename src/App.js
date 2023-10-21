a84feb1c5a8dfbb31579e4d435838705b2cc1fb8
sandeepyadav0 <sandeepyadav27577@gmail.com>
2023-10-21 12:07:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Calculator } from './components/calculator';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Calculator / >
+        <
+        /div>
+    );
+}
+
+export default App;
